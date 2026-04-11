fe92c3a91fbff7ea23ecfa4987158bfaf2d10246
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2026-04-11 06:35:15 -0700

    this is good too

--- pycroscope/checker.py
+++ pycroscope/checker.py
@@ -2123,7 +2123,7 @@ class Checker:
                 and safe_isinstance(value.val.__self__, type)
             ):
                 receiver_self = _bound_method_self_value_from_typevars(
-                    value.typevars, type(value.val.__self__)
+                    value.typevars, value.val.__self__
                 )
                 if receiver_self is not None:
                     unbound_sig = self.arg_spec_cache.get_argspec(value.val.__func__)
