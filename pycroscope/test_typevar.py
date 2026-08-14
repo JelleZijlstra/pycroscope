5e10ab49b9f815bd84734d8d476838cd4ec089bf
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2026-08-14 06:59:58 -0700

    Keep version-gated defaults test out of self-check

--- pycroscope/test_typevar.py
+++ pycroscope/test_typevar.py
@@ -673,35 +673,39 @@ class TestTypeVar(TestNameCheckVisitorBase):
         """)
 
     @skip_before((3, 11))
-    @assert_passes(allow_import_failures=True)
     def test_defaulted_paramspec_after_typevartuple(self):
-        from typing import Callable, Generic
+        self.assert_passes(
+            """
+            from typing import Callable, Generic
 
-        from typing_extensions import ParamSpec, TypeVarTuple, Unpack, assert_type
+            from typing_extensions import ParamSpec, TypeVarTuple, Unpack, assert_type
 
-        Ts = TypeVarTuple("Ts")
-        DefaultTs = TypeVarTuple("DefaultTs", default=Unpack[tuple[int, str]])
-        P = ParamSpec("P", default=[float, bool])
+            Ts = TypeVarTuple("Ts")
+            DefaultTs = TypeVarTuple("DefaultTs", default=Unpack[tuple[int, str]])
+            P = ParamSpec("P", default=[float, bool])
 
-        class WithVariadics(Generic[Unpack[Ts], P]):
-            elements: tuple[Unpack[Ts]]
-            callback: Callable[P, None]
+            class WithVariadics(Generic[Unpack[Ts], P]):
+                elements: tuple[Unpack[Ts]]
+                callback: Callable[P, None]
 
-        class WithVariadicDefaults(Generic[Unpack[DefaultTs], P]):
-            elements: tuple[Unpack[DefaultTs]]
-            callback: Callable[P, None]
+            class WithVariadicDefaults(Generic[Unpack[DefaultTs], P]):
+                elements: tuple[Unpack[DefaultTs]]
+                callback: Callable[P, None]
 
-        def check(
-            defaulted: WithVariadics[int, str],
-            explicit: WithVariadics[int, str, [bytes]],
-            defaulted_pack: WithVariadicDefaults[[bytes]],
-        ) -> None:
-            assert_type(defaulted.elements, tuple[int, str])
-            assert_type(defaulted.callback, Callable[[float, bool], None])
-            assert_type(explicit.elements, tuple[int, str])
-            assert_type(explicit.callback, Callable[[bytes], None])
-            assert_type(defaulted_pack.elements, tuple[int, str])
-            assert_type(defaulted_pack.callback, Callable[[bytes], None])
+            def check(
+                defaulted: WithVariadics[int, str],
+                explicit: WithVariadics[int, str, [bytes]],
+                defaulted_pack: WithVariadicDefaults[[bytes]],
+            ) -> None:
+                assert_type(defaulted.elements, tuple[int, str])
+                assert_type(defaulted.callback, Callable[[float, bool], None])
+                assert_type(explicit.elements, tuple[int, str])
+                assert_type(explicit.callback, Callable[[bytes], None])
+                assert_type(defaulted_pack.elements, tuple[int, str])
+                assert_type(defaulted_pack.callback, Callable[[bytes], None])
+            """,
+            allow_import_failures=True,
+        )
 
     @skip_before((3, 11))
     def test_typevartuple_unpack_assignability(self):
